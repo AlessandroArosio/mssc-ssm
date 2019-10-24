7de41b558383ec2e712d07ac67649864542eaa07
Alessandro Arosio <alessandro.arosio1@gmail.com>
2019-10-24 19:17:44 +0100

    configuring state machine with enums

---
+++ src/main/java/guru/springframework/msscssm/domain/PaymentEvent.java
@@ -0,0 +1,5 @@
+package guru.springframework.msscssm.domain;
+
+public enum PaymentEvent {
+  PRE_AUTHORISE, PRE_AUTH_APPROVED, PRE_AUTH_DECLINED, AUTHORISE, AUTH_APPROVED, AUTH_DECLINED
+}
---
+++ src/main/java/guru/springframework/msscssm/repository/PaymentRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.msscssm.repository;
+
+import guru.springframework.msscssm.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}

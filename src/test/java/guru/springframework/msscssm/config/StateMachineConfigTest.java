19f642d51807ae31d13bf1a0b136e9d37086aa31
Alessandro Arosio <alessandro.arosio1@gmail.com>
2019-10-27 18:08:38 +0000

    configuring state machine and listener

---
+++ src/test/java/guru/springframework/msscssm/config/StateMachineConfigTest.java
@@ -0,0 +1,36 @@
+package guru.springframework.msscssm.config;
+
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StateMachineConfigTest {
+
+  @Autowired
+  StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+  @Test
+  public void testNewStateMachine() {
+    StateMachine<PaymentState, PaymentEvent> sm = factory.getStateMachine(UUID.randomUUID());
+    sm.start();
+
+    System.out.println(sm.getState().toString());
+
+    sm.sendEvent(PaymentEvent.PRE_AUTHORISE);
+
+    System.out.println(sm.getState().toString());
+
+    sm.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
+
+    System.out.println(sm.getState().toString());
+  }
+}

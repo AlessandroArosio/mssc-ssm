4515c7fffc780819ee5d5d1712157034da5a029a
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-01-21 18:36:16 +0000

    Initialising State Machine from database

--- src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
+++ src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
@@ -7,6 +7,7 @@ import guru.springframework.msscssm.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,16 +25,37 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        var sm = build(paymentId);
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> authorisePayment(Long paymentId) {
+        var sm = build(paymentId);
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+        var sm = build(paymentId);
         return null;
     }
+
+    // this restore the status of the StateMachine from the DB
+    private StateMachine<PaymentState, PaymentEvent> build(Long paymentId) {
+        var payment = paymentRepository.getOne(paymentId);
+
+        var sm = stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+
+        sm.stop();
+
+        sm.getStateMachineAccessor()
+                .doWithAllRegions(sma ->
+                        sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null)));
+
+        sm.start();
+
+        return sm;
+    }
+
 }

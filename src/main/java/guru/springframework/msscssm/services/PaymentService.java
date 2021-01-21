b2ba2d941ab20cc07e0967c7dfadc765e765b28f
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-01-21 18:23:10 +0000

    Creating payment service

---
+++ src/main/java/guru/springframework/msscssm/services/PaymentService.java
@@ -0,0 +1,17 @@
+package guru.springframework.msscssm.services;
+
+import guru.springframework.msscssm.domain.Payment;
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import org.springframework.statemachine.StateMachine;
+
+public interface PaymentService {
+
+    Payment newPayment(Payment payment);
+
+    StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> authorisePayment(Long paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId);
+}
